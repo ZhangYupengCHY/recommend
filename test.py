3e297ddbe6fbba11235ef3c6130ce514ef08a35a
张于鹏 <1579922399@qq.com>
2020-07-24 17:32:24 +0800

    123

--- test.py
+++ test.py
@@ -6,3 +6,5 @@ path = r"C:\Users\Administrator\Desktop\utf8_ad_data2020072101-603.csv"
 file_data = pd.read_csv(path, sep=',', error_bad_lines=False,warn_bad_lines=False)
 print(file_data.columns)
 print(file_data.head(2))
+print(file_data.head(2))
+
